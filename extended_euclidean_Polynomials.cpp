9dac1e45af64cadb3c4d70a44829af26cb55549d
tymroz <135758015+tymroz@users.noreply.github.com>
2024-07-15 14:31:16 +0200

    Update extended_euclidean_Polynomials.cpp

--- extended_euclidean_Polynomials.cpp
+++ extended_euclidean_Polynomials.cpp
@@ -32,18 +32,26 @@ std::pair<Polynomial, Polynomial> dividePolynomials(Polynomial dividend, Polynom
             remainder.coefficients.pop_back();
         }
     }
+    while(quotient.coefficients.size() <= divisorDegree){
+        quotient.coefficients.insert(quotient.coefficients.begin(), 0);
+    }
     
     return std::make_pair(quotient, remainder);
 }
 
 void printPolynomial(Polynomial& poly) {
     int degree = poly.coefficients.size() - 1;
+    bool first = true;
     for (int i = degree; i >= 0; --i) {
         if (poly.coefficients[i] != 0) {
-            std::cout << poly.coefficients[i] << "x^" << i;
-            if (i != 0) {
+            if (!first && poly.coefficients[i] > 0) {
                 std::cout << " + ";
             }
+            std::cout << poly.coefficients[i];
+            if (i > 0) {
+                std::cout << "x^" << i;
+            }
+            first = false;
         }
     }
     std::cout << std::endl;
@@ -65,11 +73,11 @@ Polynomial multiplyPolynomials(Polynomial a, Polynomial b) {
 Polynomial subtractPolynomials(Polynomial a, Polynomial b){
     Polynomial result;
         size_t resultSize = std::max(a.coefficients.size(), b.coefficients.size());
-        result.coefficients.resize(resultSize, 0.0);
+        result.coefficients.resize(resultSize, 0);
 
         for (size_t i = 0; i < resultSize; ++i) {
-            double coefa = (i < a.coefficients.size()) ? a.coefficients[i] : 0.0;
-            double coefb = (i < b.coefficients.size()) ? b.coefficients[i] : 0.0;
+            double coefa = (i < a.coefficients.size()) ? a.coefficients[i] : 0;
+            double coefb = (i < b.coefficients.size()) ? b.coefficients[i] : 0;
             result.coefficients[i] = coefa - coefb;
         }
 
@@ -105,22 +113,24 @@ Extended_GCD extendedEuclidean(Polynomial f, Polynomial g){
 }
 
 int main() {
-    Polynomial f, g;
-    
-    f.coefficients = {2, 2, 2};
-    g.coefficients = {1, 1, 1};
-    
-    printPolynomial(f);
-    printPolynomial(g);
+    Polynomial f, g, h, i;
 
     /*
+    przyklad:
     f = 2x^2 + 2x + 2
     g = x^2 + 1x + 1
     NWD(f,g) = x^2 + 1x + 1
     A = 0
     B = 1
-    */
+     */
+    f.coefficients = {2, 2, 2};
+    g.coefficients = {1, 1, 1};
     
+    std::cout << "f: ";
+    printPolynomial(f);
+    std::cout << "g: ";
+    printPolynomial(g);
+
     Extended_GCD wynik = extendedEuclidean(f, g);
 
     std::cout << "NWD: ";
@@ -129,6 +139,32 @@ int main() {
     printPolynomial(wynik.A);
     std::cout << "B: ";
     printPolynomial(wynik.B);
+    std::cout << std::endl;
+   
+    /*
+    przyklad:
+    h = x^2 + x + 1
+    i = x + 1
+    NWD(f,g) = 1
+    A = 1
+    B = -x
+    */
+    h.coefficients = {1, 1, 1};
+    i.coefficients = {1, 1};
+    
+    std::cout << "f: ";
+    printPolynomial(h);
+    std::cout << "g: ";
+    printPolynomial(i);
+
+    Extended_GCD wynik2 = extendedEuclidean(h, i);
+
+    std::cout << "NWD: ";
+    printPolynomial(wynik2.gcd);
+    std::cout << "A: ";
+    printPolynomial(wynik2.A);
+    std::cout << "B: ";
+    printPolynomial(wynik2.B);
     
     return 0;
 }
